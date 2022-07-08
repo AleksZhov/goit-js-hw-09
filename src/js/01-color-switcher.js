fa87608282ac09f88cae40def2bde20ade88831f
AleksZhov <alekszhov@gmail.com>
2022-07-08 19:16:29 +0300

    Initial commit

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,5 @@
+const btnStartColorChangeRef = document.querySelector('button[data-start]');
+const btnStopColorChangeRef = document.querySelector('button[data-stop]');
+
+btnStartColorChangeRef.addEventListener('click', startChangeBodyColor);
+btnStopColorChangeRef.addEventListener('click', startChangeBodyColor);
