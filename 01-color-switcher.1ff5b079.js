0ce1d8d67141cfe9e1864e531bc988badcb61593
AleksZhov <103404296+AleksZhov@users.noreply.github.com>
2022-07-08 16:17:09 +0000

    Deploying to gh-pages from @ AleksZhov/goit-js-hw-09@fa87608282ac09f88cae40def2bde20ade88831f 🚀

---
+++ 01-color-switcher.1ff5b079.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",startChangeBodyColor),e.addEventListener("click",startChangeBodyColor);
+//# sourceMappingURL=01-color-switcher.1ff5b079.js.map
---
+++ 01-color-switcher.c5f0e48a.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",startChangeBodyColor),e.addEventListener("click",startChangeBodyColor)}();
+//# sourceMappingURL=01-color-switcher.c5f0e48a.js.map
