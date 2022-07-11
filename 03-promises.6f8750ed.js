3599d82a5faf5e006775e6a630fc55054a7cb1c2
AleksZhov <103404296+AleksZhov@users.noreply.github.com>
2022-07-11 10:06:34 +0000

    Deploying to gh-pages from @ AleksZhov/goit-js-hw-09@7ccc08150eef759677edac5606b301c808eeef1c 🚀

--- 03-promises.6f8750ed.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".form");document.querySelector('button[type="submit"]');e.addEventListener("submit",(function(e){console.log(e.currentTarget.values)}));
-//# sourceMappingURL=03-promises.6f8750ed.js.map
--- 03-promises.ca461e4c.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".form");document.querySelector('button[type="submit"]');e.addEventListener("submit",(function(e){console.log(e.currentTarget.values)}))}();
-//# sourceMappingURL=03-promises.ca461e4c.js.map
